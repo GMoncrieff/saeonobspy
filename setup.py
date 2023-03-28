e354aa37cb15c4be2b613f49108741bee5852b83
Glenn Moncrieff <glennwithtwons@gmail.com>
2023-03-28 21:47:18 +0200

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='saeonobspy',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'geopandas',
+        'requests',
+        'shapely'
+    ],
+    url='https://github.com/GMoncrieff/saeonobspy',
+    author='Glenn Moncrieff',
+    author_email='glenn@saeon.ac.za',
+    description='Python package to interact with the SAEON Observation Database API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
+
+pytest
+pytest-mock
+geopandas
+shapely
+pandas
+requests
+coverage
+setuptools
